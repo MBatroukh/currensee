9f88f271a6bace4bc1f2d135475286b3b4ffd58d
Mohammad <mohammad.batroukh@soulfx.com>
2018-12-23 15:33:28 -0500

    removed package-lock.json

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 const server = require('./api/server');
 const { PORT, DB_URI } = require('./api/utils/constants');
 
-server.listen(PORT, async () => {
-    await mongoose.connect(DB_URI)
+await mongoose.connect(DB_URI)
+
+server.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
     console.log(`Database Connected at ${DB_URI}`)
 });
