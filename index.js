568a0fd85e7964013be34eab7dba498a3cb2c7c8
Mohammad <mohammad.batroukh@soulfx.com>
2019-01-05 13:22:05 -0500

    minor updates

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const mongoose = require('mongoose');
 const server = require('./api/server');
 const { PORT, DB_URI } = require('./api/utils/constants');
 
-await mongoose.connect(DB_URI)
-
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+    await mongoose.connect(DB_URI)
     console.log(`App listening on port ${PORT}`)
     console.log(`Database Connected at ${DB_URI}`)
 });
